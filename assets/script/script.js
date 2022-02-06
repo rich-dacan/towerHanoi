c3aa9c2d02620ff668b1f08a96f88bf10fb3e776
Richard Cândido <rich_dacan@protonmail.com>
2022-02-06 15:35:41 -0300

    create: logic game

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,8 +1,9 @@
 
-// Para esse projeto preciso criar elemento via DOM: torres e discos;
+// Para esse projeto preciso criar elemento via DOM: torres e discos; OK
 // Apenas um disco pode ser movido por vez. (addEventListener) function interceptarEvent()
 // Cada movimento consiste de pegar o disco de cima de uma das pilhas e movê-lo para o topo de outra pilha.
 // Nenhum disco pode ser colocado no topo de um disco menor.  (Element.clientWidth) 
+
 
 
 // CRIANDO TORRES;
@@ -23,26 +24,167 @@ const createTowers = () => {
 createTowers()
 
 // CRIANDO DISCOS;
-const disks = ["disco5","disco4","disco3", "disco2", "disco1"];
+function createDisks(currentDiskArr) {
 
-disks.forEach(createDisks);
+  const disk = document.createElement('div');
+  disk.id    = currentDiskArr;
+  disk.innerText = currentDiskArr;
 
-function createDisks(discoID){
-
-  const disco = document.createElement('div');
-  disco.id    = discoID;
-
-  const torre = document.getElementById("tower1");
+  const tower = document.getElementById("tower1");
  
-  torre.appendChild(disco);
+  tower.appendChild(disk);
   
 }
 
+// TRABALHANDO AS MOVIMENTAÇÕES;
+let tower;
+let disk;
+
+let towerOrigin;
+let towerFinal;
+
+let diskOrigin;
+let diskFinal;
+let widthDiskOrigin = 0;
+let widthDiskFinal  = 0;
+
+let clickCounter = 0;
+let movCount     = 0;
+
+// HANDLER DE CLIQUE EM CADA TORRE, IDENTIFICANDO-A;
+function clickTower(event) {
+  tower = event.currentTarget;
+  clickCounter++;
+  console.log(tower);
+  receivingTower();
+}
+
+const tower1 = document.getElementById('tower1');
+const tower2 = document.getElementById('tower2');
+const tower3 = document.getElementById('tower3');
+
+tower1.addEventListener('click', clickTower);
+tower2.addEventListener('click', clickTower);
+tower3.addEventListener('click', clickTower);
+
+// FUNÇÃO CERNE DAS MOVIMENTAÇÕES;
+function receivingTower() {
+  if (clickCounter % 2 === 1) {
+      towerOrigin = tower;
+      diskOrigin = towerOrigin.lastElementChild;
+      widthDiskOrigin = diskOrigin.clientWidth;
+  } else {
+      towerFinal = tower;
+      if (towerFinal.childElementCount !== 0) {
+          diskFinal = towerFinal.lastElementChild;
+          widthDiskFinal = diskFinal.clientWidth;
+      }
+      diskMove()
+  }
+  
+}
+function diskMove () {
+
+  if (widthDiskOrigin < widthDiskFinal || towerFinal.childElementCount === 0) {
+      towerFinal.appendChild(diskOrigin);
+      movCount++
+      movCounter.innerHTML= `Movement Counter: ${movCount}`
+ }
+}
+
+const movCounter = document.getElementById('counterMov');
+movCounter.innerText = 'Movement Counter: 0 ';
+
+// NÍVEIS DE DIFICULDADE;
+
+let smallDisk = '';
+
+// EASY MODE!
+const easyMode = ['disk5', 'disk3', 'disk1']
+
+function modeEasy (){
+  for(let i = 0; i < 3; i++) {
+
+    createDisks(easyMode[i]);
+    
+  }
+
+}
+const easy = document.getElementById('easy');
+easy.addEventListener('click', modeEasy);
+
+// MEDIUM MODE!
+const mediumMode = ['disk4','disk3', 'disk2', 'disk1']
+
+function modeMedium() {
+  for(let i = 0; i < 4; i++) {
+    createDisks(mediumMode[i]);
+  }
+}
+
+const medium = document.getElementById('medium');
+medium.addEventListener('click', modeMedium);
+
+// HARD MODE!
+const hardMode = ['disk5','disk4','disk3', 'disk2', 'disk1'];
+
+function modeHard() {
+  for(let i = 0; i < 5; i++) {
+    createDisks(hardMode[i]);
+  }
+}
+
+const hard = document.getElementById('hard');
+hard.addEventListener('click', modeHard);
+
+// FUCKING HARDCORE!
+const fuckHard = ['disk7','disk6','disk5','disk4','disk3', 'disk2', 'disk1'];
+
+function modeFuck() {
+  for(let i = 0; i < 7; i++) {
+    createDisks(fuckHard[i]);
+    
+  }
+}
+
+const fuck = document.getElementById('fuck');
+fuck.addEventListener('click', modeFuck);
 
 
 
 
 
+
+
+
+
+// CRIANDO UM ESCUTADOR DE CLIQUES EM TODO O CONTAINER (ÁREA CLICÁVEL);
+// const btnContainer = document.getElementById('container');
+
+// btnContainer.addEventListener('click', () => {
+  
+//   // let disco = document.getElementById('disk1');
+//   // disco.innerText = 'Disk 1';
+  
+//   // disco.style.lineHeight = '100%'
+
+//   // disco.style.marginBottom = '360px';
+
+// } );
+
+// const diskColor = document.getElementById('nivel-dificuldade');
+// diskColor.addEventListener('click', colorsDisks);
+
+// function colorsDisks() {
+//   let disk1 = document.getElementById('disk1');
+//   disk1.style.background = 'purple';
+//   let disk2 = document.getElementById('disk2');
+//   disk2.style.background = 'yellow';
+//   let disk3 = document.getElementById('disk3');
+//   disk3.style.background = 'red';
+
+
+// }
 
 
 /*
